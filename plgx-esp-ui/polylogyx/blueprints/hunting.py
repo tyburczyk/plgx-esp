a5cebf24e374117c02900017a72c2ad8b34a91fc
Jacek Tyburczyk <jtyburczyk@eclecticiq.com>
2020-09-03 11:37:38 +0200

    updating hunt endpoints

--- plgx-esp-ui/polylogyx/blueprints/hunting.py
+++ plgx-esp-ui/polylogyx/blueprints/hunting.py
@@ -23,7 +23,6 @@ class PackAlerts(Resource):
     @ns.expect(parser)
     def post(self, pack_id):
         args = self.parser.parse_args()
-        # TODO: move it to dao?
         # TODO: rename in the pack:
         pack_id = f"hunting-pack--{pack_id}"
         rules = Rule.query.join(Rule.alerts).filter(Rule.name.startswith(pack_id))
@@ -35,42 +34,40 @@ class PackAlerts(Resource):
         if args["end_time"]:
             rules = rules.filter(Alerts.created_at <= args["end_time"])
 
-        rules = rules.options(db.contains_eager(Rule.alerts)).all()
-        data = {
-            rule.name: [
-                {
-                    "node_id": alert.node_id
-                }
-                for alert in rule.alerts
-            ] for rule in rules
-        }
+        data = {}
+        for rule in rules.options(db.contains_eager(Rule.alerts)).all():
+            # TODO make it more dry (if possible)
+            technique_id, query_id = rule.name.split('--')[2].split('.')
+            rule_name = rule.description.split('--')[2]
+            # TODO: get relevant alert data here:
+            data[rule_name] = {
+                "technique_id": technique_id,
+                "query_id": query_id,
+                "alerts": [alert.to_dict() for alert in rule.alerts]
+            }
 
         return jsonify(data)
 
 
 @require_api_key
-@ns.route('/query/<string:query_id>/results', endpoint="query_results")
+@ns.route('/query/<string:technique_id>/<int:query_id>/results', endpoint="query_results")
 @ns.doc(params={'host_identifier': 'Host identifier of the Node', 'start_time': 'start time of alerted events', 'end_time': 'end time of alerted events'})
-class PackAlerts(Resource):
+class QueryResults(Resource):
     parser = requestparse(['host_identifier', 'start_time', 'end_time'],
                           [str, datetime.datetime.fromtimestamp, datetime.datetime.fromtimestamp],
                           ["host identifier of the node", "start time of alerted events", "end time of alerted events"],
                           [False, False, False])
 
     @ns.expect(parser)
-    def post(self, query_id):
+    def post(self, technique_id, query_id):
         args = self.parser.parse_args()
-        # TODO: consider query_id in the url to only be technique_id.query_id, without pack part in it
         # TODO: add some nice validation (after cleaning naming)
-        # query name in pack currently looks like this: "hunting-pack--9fff2972-2243-4cb3-a947-58f20fec17be--T1060.1"
-        query_id = query_id.split('--')[2]
-        # query name in esp currently looks like this: "pack/eiq_xdr_windows/test_T1060.1"
-        query_name = f"pack/hunting_queries/test_{query_id}"
+        query_name = f"pack/hunting_queries/test_{technique_id}.{query_id}"
+
         results = ResultLog.query.filter(ResultLog.name == query_name)
         # TODO: use some node id + join it and not pk
         if args["host_identifier"]:
             results = results.filter(ResultLog.node_id == args["host_identifier"])
-        # TODO: probably timestamp is not of event occurence
         if args["start_time"]:
             rules = results.filter(ResultLog.timestamp >= args["start_time"])
         if args["end_time"]:
